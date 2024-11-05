2103975e45e33d37f19e9f83010451c1ed9e3f92
kodt2 <kodt2005@yandex.ru>
2024-11-05 21:30:10 +0300

    work list@

--- include/tlist.h
+++ include/tlist.h
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 template<typename T>
-class list {
+class Tlist {
 private:
 	struct Node {
 		T data;
@@ -8,10 +8,13 @@ private:
 	};
 	Node* first;
 public:
-	list() {
+	Tlist() {
 		first = nullptr;
 	};
-	list(int n, T deflt = T()) {
+	Tlist(int n, T deflt = T()) {
+		if (n < 0) {
+			throw "eogts";
+		}
 		Node* temp = new Node();
 		temp->data = deflt;
 		temp->next = nullptr;
@@ -25,50 +28,62 @@ public:
 			current = temp;
 		}
 	}
-	~list() {
+	~Tlist() {
 		Node* current = first;
 		Node* temp;
-		while (current) {
+		while (current!=nullptr) {
 			temp = current;
 			current = current->next;
 			delete temp;
 		}
 		first = nullptr;
 	};
-	list(const list&) {
-		delete this;
-		Node* current_old = list.get_first();
+	Tlist(const Tlist& list) {
+		Node* current_old = list.first;
 		Node* temp = new Node();
 		temp->data = current_old->data;
 		temp->next = nullptr;
 		first = temp;
 		Node* current_new = first;
-		while (current_old) {
-			current_old = current_old->next;
+		current_old = current_old->next;
+		while (current_old!=nullptr) {
 			temp = new Node();
 			temp->data = current_old->data;
 			temp->next = nullptr;
 			current_new->next = temp;
 			current_new = temp;
-
+			current_old = current_old->next;
 		}
 	};
-	list operator= (const list&) {
-		delete this;
-		Node* current_old = list.get_first();
-		Node* temp = new Node();
-		temp->data = current_old->data;
-		temp->next = nullptr;
-		first = temp;
-		Node* current_new = first;
-		while (current_old) {
-			current_old = current_old->next;
-			temp = new Node();
-			temp->data = current_old->data;
-			temp->next = nullptr;
-			current_new->next = temp;
-			current_new = temp;
-		};
+	bool operator==(const Tlist& list) const {
+		bool res = true;
+		Node* current2 = list.first;
+		Node* current1 = first;
+		while (current1 != nullptr || current2 != nullptr) {
+			if ((current1 == nullptr && current2 != nullptr) || (current2 == nullptr && current1 != nullptr)) {
+				res = false;
+				return res;
+			}
+			if (current1->data != current2->data) {
+				res = false;
+				return res;
+			}
+			current1 = current1->next;
+			current2 = current2->next;
+		}
+		return res;
+	}
+	Tlist& operator= (const Tlist& list) {
+		if (first == list.first) {
+			return *this;
+		}
+		Tlist* res = new Tlist(list);
+		Tlist* temp = new Tlist();
+		temp->first = first;
+		delete temp;
+		first = res->first;
+		res->first = nullptr;
+		return *this;
 	}
 	void print() {
 		Node* current = first;
@@ -76,8 +91,12 @@ public:
 			std::cout << current->data << " ";
 			current = current->next;
 		}
+		std::cout << "\n";
 	};
 	T& operator[](int index) {
+		if (index<0 || index>=this->size()) {
+			throw "list index out of range";
+		}
 		Node* current_ptr = first;
 		int cur_ind = 0;
 		T res = current_ptr->data;
@@ -92,42 +111,101 @@ public:
 		return res;
 
 	};//O(n)
-	Node* insert(T value, Node* prev) {
-		Node* temp = new Node(value, prev->next);
+	void set_element(T value, int index) {
+		Node* t;
+		try {
+			t = find_ptr(index);
+		}
+		catch (...) {
+			throw  "list index out of range";
+		}
+		t->data = value;
+	}
+	void insert(T value, int index) {
+		Node* prev;
+		try {
+			prev = find_ptr(index-1);
+		} 
+		catch (...) {
+			throw  "list index out of range";
+		}
+		Node* temp = new Node();
+		temp->data = value;
+		temp->next = prev->next;
 		prev->next = temp;
-		return first;
 	};
-	Node* insert_front(T value) {
-		Node* temp = new Node(value, first);
+	void insert_front(T value) {
+		Node* temp = new Node();
+		temp->data = value;
+		temp->next = first;
 		first = temp;
-		return first;
 	};
-	Node* errase(Node* prev) {
-		Node* temp = prev->next;
+	Node* errase(int index) {
+		Node* prev;
+		try {
+			prev = find_ptr(index-1);
+		}
+		catch (...) {
+			throw  "list index out of range";
+		}
+		Node* temp = new Node();
+		temp = prev->next;
 		prev->next = temp->next;
 		delete temp;
 		return first;
 	};
 	Node* errase_front() {
 		Node* temp = first;
-		first = first->nest;
+		first = first->next;
 		delete temp;
 		return first;
 	};
 	Node* find(T value) {
-
+		Node* current = first;
+		Node* res = nullptr;
+		while (current != nullptr) {
+			if (current->data == value) {
+				res = current;
+			}
+			current = current->next;
+		}
+		return res;
 	};//O(n)
-	Node* get_first() {
-		return first;
-	};
+	Node* find_ptr(int index) {
+		if (index<0 || index>=this->size()) {
+			throw "list index out of range";
+		}
+		Node* current = first;
+		for(int i=0; i<index;i++) {
+			current = current->next;
+		}
+		return current;
+	}
 	size_t size() {
 		size_t i = 0;
 		Node* current = first;
 		while (current != nullptr) {
-			std::cout << current->data << " ";
+			//std::cout << current->data << " ";
 			current = current->next;
 			i++;
 		}
 		return i;
 	};
+
+	Tlist& merge_sorted_lists(Tlist& list) {
+		if (first == nullptr) {
+			return list;
+		}
+		if (list->first == nullptr) {
+			return *this;
+		}
+		Node* current1 = first;
+		Node* current2 = list->first;
+		if (current1->data < current2->data) {
+			Node* temp = current1->next;
+			current1->next = current2;
+			current2 = current2->next;
+			current1->next->next = temp;
+		}
+	}
 };
